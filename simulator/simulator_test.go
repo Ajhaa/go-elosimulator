aa149bf7b09d1605eb4074e354df8559ea8f79a5
atte <atte.h.1998@gmail.com>
2020-12-13 19:03:31 +0200

    More tests

--- simulator/simulator_test.go
+++ simulator/simulator_test.go
@@ -1,6 +1,7 @@
 package simulator
 
 import (
+	"math"
 	"testing"
 )
 
@@ -8,5 +9,18 @@ func TestPlayer(t *testing.T) {
 	player := CreateRandomPlayer()
 	if player.Elo != 1500 {
 		t.Errorf("Expected elo to be 1500, was %f", player.Elo)
+	}
+}
+
+func TestMatchEloChange(t *testing.T) {
+	p1 := CreateRandomPlayer()
+	p2 := CreateRandomPlayer()
+
+	Match(p1, p2)
+
+	sum := p1.Elo + p2.Elo
+
+	if math.Abs(sum-3000) > 0.01 {
+		t.Errorf("Expected elo sum to be 3000, was %f", sum)
 	}
 }
